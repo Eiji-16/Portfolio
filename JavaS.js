107f4441563c387daf5caf681d5424eb2d0f52b8
Eiji-16 <ajph305@gmail.com>
2024-07-01 13:33:53 +0800

    Add files via upload

---
+++ JavaS.js
@@ -0,0 +1,9 @@
+
+function welcomeMessage() {
+    var message = "Welcome to my portfolio!\n\n";
+    message += "Use a 0.67 initial scale/zoom in your browser to get a better experience. ";
+    alert(message);
+}
+
+
+window.onload = welcomeMessage;
